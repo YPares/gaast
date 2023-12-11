114be5ff194042dac2b4e282adf8ef1bef97f612
Ywen <yves.pares@gmail.com>
2023-12-11 12:49:19 +0100

    Rewording docs, cutting down some sentences

--- README.md
+++ README.md
@@ -5,42 +5,52 @@ before direct evaluation or code generation, in order to limit allocations and
 computations to what is needed to get the wanted result. For instance, let's
 take the following expression:
 
-$D = \langle AB + C \rangle_{2}$
+$D = \langle A + BC \rangle_{2}$
 
 where $A$, $B$ and $C$ are arbitrary multivector-valued sub-expressions or
-literals. To evaluate $D$, we will only read, allocate or compute:
+literals. To evaluate $D$, we actually only need to:
 
-- the grade 2 part of $C$
-- the parts of $A$ and $B$ which have a grade that will contribute to the grade
-  2 part of the geometric product $AB$
+- allocate the grade 2 part of $D$
+- add to it the grade 2 part of $A$
+- multiply together the parts of $B$ and $C$ of a grade that will contribute to the grade
+  2 part of the geometric product $BC$, and add that to $D$
 
-To do so, GA expressions are processed in 4 phases:
+`gaast` finds that out by itself, so the user only needs to write $D$ this way:
+
+```rust
+let d = (a + b * c).g(2);
+```
+
+Said otherwise, `gaast` exploits the grade-changing properties of GA operators
+(that can be fully known ahead of time) and their linearities. To achieve this,
+it processes GA expressions in 4 phases:
 
 - 1: Expression construction (declare input multivectors and combine them into
   an expression with the provided operators)
 - 2: AST reification (provide the metric and get an actual mutable AST)
-- 3: AST specialization (perform grade inference and resolve individual
-  component-to-component operations)
+- 3: AST specialization (perform grade inference and resolve which individual
+  component-to-component operations are actually needed)
 - 4: AST evaluation (actually read the input data and run the operations
-  resolved at the previous steps)
+  resolved at the previous step)
 
 `gaast` is still pretty experimental. It aims at applications dealing with
 metric & finite-dimensional vector spaces, but which must cope with a broad
 range of possible dimensionalities. For instance: data mining, machine learning
 and multi-dimensional signal processing, or just general GA
-teaching/visualization. It therefore does not aim at being the best fit for
-fixed and low-dimension applications like computer graphics or physics, for
-which an implementation tailored to a specific algebra and dimension (like 3D
-Plane-based or Conformal GA) would be more efficient.
+teaching/visualization. It is therefore not geared towards fixed, low-dimension
+applications like physics or computer graphics. For these, an implementation
+tailored to a specific algebra and dimension (like 3D Plane-based or Conformal
+GA) would be more efficient.
 
 Please refer to the documentation in [`lib.rs`](src/lib.rs) for more
 information.
 
-## Implemented
+## Implemented so far
 
 _When applicable, "**MV**" means that the feature is implemented for expressions
 evaluating to arbitrary multivectors, and "**VSR**" means it is only implemented
-for expressions evaluating to versors._
+for expressions evaluating to versors (ie. a multivector that can be expressed
+as a geometric product of vectors)._
 
 - Linear combinations of expressions, raw multivectors and scalar literals
   (**MV**)
@@ -67,31 +77,31 @@ done:
 ### Immediate roadmap
 
 - Versor exponentiation & logarithm
-- Change the inputs used by a SpecializedAst (ie. re-using a "precompiled"
+- Change the inputs used by a SpecializedAst (ie. re-use a "precompiled"
   expression with different inputs that respect the same "schema"). It's doable
-  right know by using a custom mutable datatype that implements the `Graded`
+  right know by using a custom mutable datatype that implements the `GradedData`
   trait, but a more convenient API should be built
-- Gram matrix diagonalization (to work on algebras with a non-diagonal metric,
-  ie. with a basis of non-orthogonal vectors)
+- Gram matrix diagonalization (to work with algebras that have a non-diagonal
+  metric, ie. with a basis of non-orthogonal vectors)
 - More extensive test suite
 - Better handling of sparsity (see next section)
 
 ### Caveats
 
 - This is Rust. Expect to use `.clone()` a lot on subexpressions that you want
-  to refer to several times in your final GA expression. Note that no memory is
-  actually copied, so it's mostly an API concern. `clone` also ensures the two
-  subexpressions will share the same identifier, which allows caching. Aside
-  from this, the API should be pretty concise, and thanks to the genericity of
-  Rust operators, the need for explicit casts should be limited.
+  to use several times in your final GA expression. Note that no memory is
+  actually copied, it's really an API concern. `clone` also ensures that the
+  cloned subexpressions share the same identifier, which allows caching. Aside
+  from this, the API should be pretty concise. Notably, thanks to the genericity
+  of Rust operators, the need for explicit casts should be fairly limited.
 - Grade sets and basis blades are represented by dynamically-sized bitvectors
   (to be agnostic of vector space dimension), which are of course much slower
   than stack-allocated bitfields like `u64`. However, this bitvector
   manipulation is limited to phases 1-2-3, so it should not impact the speed of
   the actual computations.
 - Multivectors are read and written in a "one array per grade" fashion. This
-  imposes a dense storage whatever the grade. Enabling a sparse storage for some
-  grades would be necessary for higher-dimension spaces.
+  imposes a dense (contiguous) storage whatever the grade. Enabling a sparse
+  storage for some grades would be necessary for higher-dimension spaces.
 
 ### Potential future work
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Geometric Algebra Abstract Syntax Tree
 //!
-//! Contruct geometric algebra expressions and perform grade inference on them,
+//! Construct geometric algebra expressions and perform grade inference on them,
 //! in order to limit allocations and computations to what is needed to get the
 //! wanted result. The main type to build and combine these expressions is the
 //! [`Expr`] type, which exposes methods for the geometric algebra primitives.
@@ -14,59 +14,74 @@
 //!
 //! Computing GA expressions with `gaast` is done fully at runtime, in 4 phases:
 //!
-//! - 1: **Expression construction**. Combine raw multivectors into a geometric
-//!   algebra expression using the operators and methods provided by [`Expr`].
-//!   The actual algebra is not known at this point, therefore internally, at
-//!   this step, the expressions are represented by closures, expecting to
-//!   receive an object representing an algebra to create the actual AST.
-//! - 2: **AST reification**. We feed the algebra and evaluate the
-//!   aforementioned closures. This step also does _upwards grade inference_:
-//!   the algebra and the grades of the input data are known, so we can, as we
-//!   construct the AST, infer for each subexpression the grades contained in
-//!   the multivector to which it will evaluate to.
-//! - 3: **AST specialization**. This step does _downwards grade inference_.
-//!   It's the same process than during phase 2, but in reverse: knowing the
-//!   grade(s) that our entire expression evaluates to (notably if this
+//! - 1: **Expression construction**. We combine raw multivectors into a
+//!   geometric algebra expression, using the operators and methods provided by
+//!   [`Expr`]. The actual algebra and metric are not known yet. To account for
+//!   this, expressions at this point are represented by closures ("lambdas"),
+//!   that defer the actual AST construction to when the algebra will be given.
+//! - 2: **AST reification**. We feed the algebra (metric) and call the
+//!   aforementioned closures. This step does _upwards grade inference_ at the
+//!   same time: since the algebra and the grades of the input data are known,
+//!   we can infer for each subexpression the grades that will be contained in
+//!   the multivector it should evaluate to.
+//! - 3: **AST specialization**. This step does _downwards grade inference_. It
+//!   is very similar to the process just above, only in reverse. Knowing the
+//!   grade(s) that our entire expression should evaluate to (notably if this
 //!   expression uses some grade projections), we propagate the wanted grades
-//!   downwards so as to update each AST node with only the grades that will
-//!   need to be read or computed in the end. Individual component-to-component
-//!   multiplications to perform for each multivector product are fully resolved
-//!   at this phase, and therefore the metric is no longer needed after it.
-//! - 4: **AST evaluation**. Given the annotated AST resulting from the previous
-//!   phases, allocates, reads and computes only what is needed to get the final
+//!   downwards. We update each AST node, annotating it with only the grades
+//!   that will need to be read or computed in the end. We also resolve the
+//!   actual individual component-to-component multiplications to perform for
+//!   each multivector product along the way. Therefore the algebra is no longer
+//!   needed after this phase.
+//! - 4: **AST evaluation**. Given the annotated AST resulting from the above,
+//!   we now read, allocate and compute only what is needed to get the final
 //!   multivector to which the full AST evaluates to.
 //!
-//! To mark these various stages, different types are used to reflect the result
-//! of each phase. Phases 2 & 3 are done by the [`Expr::specialize`] function,
-//! which transforms the [`Expr`] into a [`SpecializedAst`], so trying to
-//! evaluate an expression that hasn't been specialized yet will result in a
-//! compile-time type error.
+//! Different types are used to represent the result of each phase. Phases 2 & 3
+//! are both done by the [`Expr::specialize`] function, which transforms the
+//! [`Expr`] into a [`SpecializedAst`]. Thus trying to evaluate an expression
+//! that has not been specialized yet will result in a compile-time error.
 //!
-//! The main point of this approach is that phases 1, 2 & 3 can be cached. They
-//! don't need to access the actual data: they just need to know the grades and
-//! the metric in use. So as long as the final data remains compliant with that,
-//! the same AST can be "pre-compiled" and reused several times. In the future,
-//! codegen could be done to compile an AST after phase 3 to LLVM or to a GPU
-//! kernel or shader. Enough of the internal representation of the AST is made
-//! public so that external crates can implement this.
+//! The main point of this approach is that phases 1, 2 & 3 can be done just
+//! once. They don't need to access the actual data: they just need to know the
+//! grades and the metric in use. So as long as the final data remains compliant
+//! with that, the same AST can be "pre-compiled" and reused several times. In
+//! the future, codegen could be done to compile a specialized AST to LLVM or to
+//! a GPU kernel or shader. Enough of the internal representation of the AST is
+//! made public so that external crates can implement this.
 //!
 //! The second point of this approach is to limit the amount of primitives that
 //! must be special-cased. Basically, we can just implement a general notion of
-//! product (a combination of 2 multivectors that results in a specific set of
+//! product: a combination of 2 multivectors that results in a specific set of
 //! component-to-component multiplications, the most complete case being the
 //! geometric product for which every possible component-to-component
-//! multiplication is performed). [`Expr`] provides a generic [`Expr::product`]
-//! method to which we just give a function that will tell, for a given pair of
-//! k-vectors in the input, which grades of their geometric product are of
-//! interest to us. Operators `*` (geometric), `^` (outer), `&` (inner), `<<`
-//! (left contraction), and `>>` (right contraction) are just provided as
-//! shortcuts for the most common products. But they don't need any special
-//! implementation, because we just specify each time to `gaast` what we want,
-//! and then let it optimize away the parts we do not need.
+//! multiplication is performed. [`Expr`] provides a generic [`Expr::product`]
+//! method to which we give simple a function that will tell, for a given pair
+//! of k-vectors in the input, which grades of their geometric product are of
+//! interest to us. Then, operators like `*` (geometric product), `^` (outer
+//! product), `&` (inner product), `<<` (left contraction), and `>>` (right
+//! contraction) can simply be one-liners based on that common `product` method.
+//! `gaast` provides these operators because they are very common in
+//! applications, but they could just as easily be implemented in client code.
+//! They don't need any special implementation because we can just specify for
+//! each one what we want out of it, and let `gaast` optimize away the parts we
+//! do not need.
 //!
+//! This approach is also what lets `gaast` be agnostic of the actual
+//! representation of both input and result multivectors. For instance, if your
+//! application already has a preferred vector representation, then you don't
+//! need to convert back and forth. The input multivectors that the AST refers
+//! to can be either owned data (ie. be stored in the AST directly) or
+//! references to data stored elsewhere. Also, input multivectors can be of
+//! different types (e.g. so they can be tailored to which grades they contain)
+//! by using something like `Box<dyn GradedData>` or some custom enum, and still
+//! interoperate in the same AST. They can even be external resources read in a
+//! lazy fashion.
+//! 
 //! `gaast` will also provide exponential and logarithm of multivectors as
-//! primitives, though with some restrictions. However, these restrictions are
-//! checked before evaluation actually starts, to permit early failures.
+//! primitives, though with some restrictions. However, the nature of `gaast`
+//! enables it to check for these conditions before evaluation actually starts,
+//! which enables early failures.
 
 pub mod algebra;
 pub mod ast;
